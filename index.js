260eeaea7a5110f396181874da2dbf34a4ed5f2f
keff6 <kev.fallas@gmail.com>
2020-06-20 12:02:12 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphds = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+// Set handlebars middleware
+app.engine('handlebars', exphds());
+app.set('view engine', 'handlebars');
+
+// Set routes
+app.get('/', (req, res) => {
+  res.render('home', {
+    stuff: 'cool stuff'
+  });
+})
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
